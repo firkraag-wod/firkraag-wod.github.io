c9a2699029ce4c089e111cd3cc4a9315acf4f27c
Nico Lachmann <nicolachmann+wod@googlemail.com>
2019-10-16 18:13:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Kleines Archiv für Kampfberichte 
+
+![Firkraag](https://firkraag-wod.github.io/img/pali_firkraag.jpg "Firkraag")
+
+* [World-of-Dungeons (Darakesh)](http://darakesh.world-of-dungeons.de/wod/spiel)
+
+* [Aktuelle Kampfberichte](https://firkraag-wod.github.io/index.html)
+
+* [Plünderplan für die Abstimmung der Konfiguration](https://firkraag-wod.github.io/timetable.html)
+
+
+
+
---
+++ bin/extract.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+# removes img sources and the rateblock from a saved kb
+# for all kbs from the last three days up to today
+
+from=$(date -d '1 day ago' +'%F')
+froms=$(echo `date +'%F'`  `date -d '1 day ago' +'%F'` `date -d '2 day ago' +'%F'` `date -d '3 day ago' +'%F'` `date -d '4 day ago' +'%F'` `date -d '5 day ago' +'%F'` `date -d '6 day ago' +'%F'` `date -d '7 day ago' +'%F'`)
+
+for day in $froms ; do
+echo $day
+[ -d archive/$day ] || continue
+for file in archive/$day/*/* ; do
+echo "Minifying $file"
+cat ${file} |\
+  perl -pe 's|[\n\r]||g;' |\
+  perl -pe '
+            s|(src="data:image/(.*?)")|src=""|ig; \
+            s|(src=data:image(.*?)>)|src=""|ig; \
+            s|url[(]"data:image(.*?)"[)]|url()|ig; \
+            s|url[(]data:image(.*?)[)]|url()|ig; \
+            s|"data:image(.*?)"|""|ig; \
+            s|<div class="rateblock">(.*?)</div>||ig; \
+            s|<div class=rateblock>(.*?)</div>||sg;' > ${file}.min
+done
+done
